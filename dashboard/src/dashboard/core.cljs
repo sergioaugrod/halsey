04bfa6538bb27846b53f19ace188f0f65fd84589
Sérgio Rodrigues <sergioaugrod@gmail.com>
2016-06-23 14:02:59 -0300

    Fix things

--- dashboard/src/dashboard/core.cljs
+++ dashboard/src/dashboard/core.cljs
@@ -3,46 +3,43 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [cljs.core.async :refer [put! chan <!]]
-            [ajax.core :refer [GET POST]]))
+            [ajax.core :refer [GET POST json-response-format]]))
 
 (enable-console-print!)
 
 (def products-list (chan))
 
-(defn handlerProduct [data]
+(defn handlerProducts [data]
+  (println data)
   (go (>! products-list data)))
 
-(defn fetch-something []
-  (GET "http://localhost:8080/products" {:handler handlerProduct})
+(defn fetch-products []
+  (GET "http://localhost:8080/products" {:handler handlerProducts, :response-format (json-response-format {:keyswords? true})})
   products-list)
 
-(defn display-name [data]
-  (str data))
+(def app-state (atom {:products []}))
 
-(def app-state (atom {:products [10 20]}))
+(defn display-product [product]
+  (let [name (last (first product)) price (last (last product))]
+    (str name " - " price)))
 
-(defn product-view [data owner]
+(defn product-view [product owner]
   (reify
     om/IRenderState
     (render-state [this state]
-      (dom/div nil
-        (dom/h2 nil (display-name data))))))
+      (dom/li nil (display-product product)))))
 
 (defn products-view [data owner]
   (reify
-    om/IInitState
-    (init-state [_]
-      {:delete (chan)
-       :text ""})
     om/IWillMount
     (will-mount [_]
-      (go (let [foo (<! (fetch-something ))]
-            (om/update! data :products foo)
+      (go (let [products (<! (fetch-products ))]
+            (om/update! data :products products)
       )))
     om/IRenderState
     (render-state [this state]
       (dom/div nil
-        (dom/h2 nil "Products List")
+        (dom/h2 nil "Products")
         (apply dom/ul nil
                (om/build-all product-view (:products data)
                              {:init-state state}))))))
--- product-api/test/product_api/service_test.clj
+++ product-api/test/product_api/service_test.clj
@@ -10,25 +10,11 @@
 (deftest home-page-test
   (is (=
        (:body (response-for service :get "/"))
-       "Hello World!"))
+       "{\"message\":\"Hello World\"}"))
   (is (=
        (:headers (response-for service :get "/"))
-       {"Content-Type" "text/html;charset=UTF-8"
+       {"Content-Type" "application/json;charset=UTF-8"
         "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
         "X-Frame-Options" "DENY"
         "X-Content-Type-Options" "nosniff"
         "X-XSS-Protection" "1; mode=block"})))
-
-
-(deftest about-page-test
-  (is (.contains
-       (:body (response-for service :get "/about"))
-       "Clojure 1.8"))
-  (is (=
-       (:headers (response-for service :get "/about"))
-       {"Content-Type" "text/html;charset=UTF-8"
-        "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
-        "X-Frame-Options" "DENY"
-        "X-Content-Type-Options" "nosniff"
-        "X-XSS-Protection" "1; mode=block"})))
-
